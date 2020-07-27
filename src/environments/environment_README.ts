a5a02afd286a562b68fcec89645c135f538060b3
rienbouw <rienbouw@gmail.com>
2020-07-27 14:05:37 +0200

    readme

---
+++ src/environments/environment_README.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  googleMapsAPIKey: "<your api key>",
+  firebase: {
+    apiKey: "<your api key>",
+    authDomain: "easygeo.firebaseapp.com",
+    databaseURL: "https://easygeo.firebaseio.com",
+    projectId: "easygeo",
+    storageBucket: "easygeo.appspot.com",
+    messagingSenderId: "<your id>",
+    appId: "<your id>",
+    measurementId: "<your id>"
+  }
+};
