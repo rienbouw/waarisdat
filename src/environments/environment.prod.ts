fc20adf112cf80c69ccd03601f2daaf152d4a5c6
rienbouw <rienbouw@gmail.com>
2020-07-27 13:36:02 +0200

    completereset

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: "<your api key>",
+  googleMapsAPIKey: "AIzaSyDlWT67WWxzXvcIKXZ6khxTIIzt9UU7rgk",
   firebase: {
-    apiKey: "<your api key>",
+    apiKey: "AIzaSyB99EbgwSf-fLqg1O2xeIxtLDXUaDiuNgc",
     authDomain: "easygeo.firebaseapp.com",
     databaseURL: "https://easygeo.firebaseio.com",
     projectId: "easygeo",
     storageBucket: "easygeo.appspot.com",
-    messagingSenderId: "<your id>",
-    appId: "<your id>",
-    measurementId: "<your id>"
+    messagingSenderId: "1060889360759",
+    appId: "1:1060889360759:web:7d64c342cf756e54d84972",
+    measurementId: "G-3MZE8SM56W"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
   production: false,
-  googleMapsAPIKey: "<your id>k",
+  googleMapsAPIKey: "AIzaSyDlWT67WWxzXvcIKXZ6khxTIIzt9UU7rgk",
   firebase: {
-    apiKey: "<your id>",
+    apiKey: "AIzaSyB99EbgwSf-fLqg1O2xeIxtLDXUaDiuNgc",
     authDomain: "easygeo.firebaseapp.com",
     databaseURL: "https://easygeo.firebaseio.com",
     projectId: "easygeo",
     storageBucket: "easygeo.appspot.com",
-    messagingSenderId: "<your id>",
-    appId: "<your id>",
-    measurementId: "<your id>"
+    messagingSenderId: "1060889360759",
+    appId: "1:1060889360759:web:7d64c342cf756e54d84972",
+    measurementId: "G-3MZE8SM56W"
   }
 };
 
