ab65626ee43f3c30b36475cab0d81b04dddc8f2d
rienbouw <rienbouw@gmail.com>
2020-07-27 12:24:19 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: "AIzaSyDlWT67WWxzXvcIKXZ6khxTIIzt9UU7rgk",
+  googleMapsAPIKey: "<your api key>",
   firebase: {
-    apiKey: "AIzaSyB99EbgwSf-fLqg1O2xeIxtLDXUaDiuNgc",
+    apiKey: "<your api key>",
     authDomain: "easygeo.firebaseapp.com",
     databaseURL: "https://easygeo.firebaseio.com",
     projectId: "easygeo",
     storageBucket: "easygeo.appspot.com",
-    messagingSenderId: "1060889360759",
-    appId: "1:1060889360759:web:7d64c342cf756e54d84972",
-    measurementId: "G-3MZE8SM56W"
+    messagingSenderId: "<your id>",
+    appId: "<your id>",
+    measurementId: "<your id>"
   }
 };
